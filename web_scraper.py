6f70a25f4a8f9969fac509e1349c7c58e881aeda
nacho <nks5@illinois.edu>
2019-09-27 18:16:26 -0500

    web_scraper.py

---
+++ web_scraper.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+URL = "https://www.noxinfluencer.com/youtube-channel-rank/top-100-all-all-youtuber-sorted-by-subs-weekly"
+r = requests.get(URL)
+soup = BeautifulSoup(r.content, 'html5lib')
+bodies = soup.findAll("span", {"class": "num"})
+print(bodies)
